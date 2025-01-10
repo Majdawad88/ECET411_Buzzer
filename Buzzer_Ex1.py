924f0b5cfd406172935d2cf5b5d1228304e9a6a0
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2025-01-09 21:00:25 -0500

    Update Buzzer_Ex1.py

--- Buzzer_Ex1.py
+++ Buzzer_Ex1.py
@@ -1,22 +1,39 @@
 
 # git clone https://github.com/Majdawad88/ECET411_Buzzer.git
 
-
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep	# Import the sleep function from the time module GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering BUZZER_PIN = 21
-GPIO.setup(BUZZER_PIN, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+from time import sleep  # Import the sleep function from the time module GPIO.setwarnings(False) # Ign>
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+BUZZER_PIN = 23
+GPIO.setup(BUZZER_PIN, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial val>
 
- 
+def BuzzerFreqControl(freq, time):
+        print("Freq = "+ str(freq)+ " time = "+ str(time))
+        if freq > 0:
+                for i in range(int((freq/2)*time)):
+                        GPIO.output(BUZZER_PIN, GPIO.HIGH)
+                        sleep(1/freq)
+                        GPIO.output(BUZZER_PIN, GPIO.LOW)
+                        sleep(1/freq)
+        else:
+
+                sleep(time)
+
 try:
- 
 
-	while True: # Run forever
-		GPIO.output(BUZZER_PIN, GPIO.HIGH) # Turn on print(' BUZZER ON')
-		sleep(1)	# Sleep for 1 second
-		GPIO.output(BUZZER_PIN, GPIO.LOW) # Turn off print(' BUZZER OFF')
-		sleep(1)	# Sleep for 1 second
- 
+
+        while True: # Run forever
+
+                BuzzerFreqControl(250, 2)
+                BuzzerFreqControl(500, 2)
+                BuzzerFreqControl(750, 2)
+                BuzzerFreqControl(0, 2)
+
+
+
 except KeyboardInterrupt:
-	print("Measurement stopped by User") 
-	GPIO.cleanup()
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+
+
+
